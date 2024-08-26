fedf00fb2fd0465082121fac6d9d953f3e3a1e16
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-08-26 16:37:07 +0300

    37. iOS navigation

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,8 +3,25 @@ import Shared
 
 struct ContentView: View {
     
+    @State private var shouldOpenAbout = false
+    
     var body: some View {
-        ArticlesScreen(viewModel: .init())
+        NavigationStack {
+            ArticlesScreen(viewModel: .init())
+                .toolbar {
+                    ToolbarItem {
+                        Button {
+                            shouldOpenAbout = true
+                        } label: {
+                            Label("About", systemImage: "info.circle")
+                                .labelStyle(.titleAndIcon)
+                        }
+                        .popover(isPresented: $shouldOpenAbout) {
+                            AboutScreen()
+                        }
+                    }
+                }
+        }
     }
 }
 
