d70b502c02f1997152f0137d62ed0a63ffd07897
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-08-27 10:26:31 +0300

    46. Articles Service

---
+++ shared/src/commonMain/kotlin/my/daily/pulse/articles/ArticlesService.kt
@@ -0,0 +1,20 @@
+package my.daily.pulse.articles
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+
+class ArticlesService(private val httpClient: HttpClient) {
+
+    private val country = "us"
+    private val category = ""
+    private val apiKey = "28376d163d03437b84e2b29b8402e338"
+
+    suspend fun fetchArticles(): List<ArticleRaw> {
+        val response: ArticlesResponse = httpClient
+            .get("https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=$apiKey")
+            .body()
+
+        return response.articles
+    }
+}
