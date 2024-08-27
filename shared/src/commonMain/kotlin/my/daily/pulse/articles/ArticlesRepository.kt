2b723e9566a9928fe107a815c35db05826bf6693
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-08-28 00:52:27 +0300

    71. Articles Repository

---
+++ shared/src/commonMain/kotlin/my/daily/pulse/articles/ArticlesRepository.kt
@@ -0,0 +1,21 @@
+package my.daily.pulse.articles
+
+class ArticlesRepository(
+    private val dataSource: ArticlesDataSource,
+    private val service: ArticlesService
+) {
+
+    suspend fun getArticles(): List<ArticleRaw> {
+        val articlesDb = dataSource.getAllArticles()
+
+        if (articlesDb.isEmpty()) {
+            val fetchedArticles = service.fetchArticles()
+
+            dataSource.insertArticles(fetchedArticles)
+
+            return fetchedArticles
+        }
+
+        return articlesDb
+    }
+}
