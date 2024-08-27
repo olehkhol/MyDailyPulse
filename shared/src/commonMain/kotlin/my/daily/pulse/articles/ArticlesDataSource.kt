ec56c2881d5d900574699fcdb687a6191c27ca0d
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-08-28 00:45:49 +0300

    70. Articles DataSource Part 2

--- shared/src/commonMain/kotlin/my/daily/pulse/articles/ArticlesDataSource.kt
+++ shared/src/commonMain/kotlin/my/daily/pulse/articles/ArticlesDataSource.kt
@@ -9,6 +9,26 @@ class ArticlesDataSource(private val database: DailyPulseDatabase) {
             .selectAllArticles(::mapToArticleRaw)
             .executeAsList()
 
+    fun insertArticles(articles: List<ArticleRaw>) {
+        database.dailyPulseDatabaseQueries.transaction {
+            articles.forEach { articleRaw ->
+                insertArticle(articleRaw)
+            }
+        }
+    }
+
+    fun clearArticles() =
+        database.dailyPulseDatabaseQueries.removeAllArticles()
+
+    private fun insertArticle(articleRaw: ArticleRaw) {
+        database.dailyPulseDatabaseQueries.insertArticle(
+            articleRaw.title,
+            articleRaw.desc,
+            articleRaw.date,
+            articleRaw.imageUrl
+        )
+    }
+
     private fun mapToArticleRaw(
         title: String,
         desc: String?,
