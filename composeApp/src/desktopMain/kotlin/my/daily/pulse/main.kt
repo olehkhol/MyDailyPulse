304a4a6d644ed087e2d96347f147a9d2018c8892
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-08-29 02:02:18 +0300

    Remove redundant ViewModelsModule

--- composeApp/src/desktopMain/kotlin/my/daily/pulse/main.kt
+++ composeApp/src/desktopMain/kotlin/my/daily/pulse/main.kt
@@ -4,13 +4,12 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import my.daily.pulse.di.databaseModule
 import my.daily.pulse.di.sharedKoinModules
-import my.daily.pulse.di.viewModelsModule
 import org.koin.core.context.startKoin
 
 fun main() {
 
     startKoin {
-        modules(sharedKoinModules + viewModelsModule + databaseModule)
+        modules(sharedKoinModules + databaseModule)
     }
 
     return application {
