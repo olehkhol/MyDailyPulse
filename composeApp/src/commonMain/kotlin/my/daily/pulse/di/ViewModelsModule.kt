b5c84ee5fdd383e11b61334a965b44273e2b6fe4
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-08-29 02:00:28 +0300

    Add SourcesInjector

--- composeApp/src/commonMain/kotlin/my/daily/pulse/di/ViewModelsModule.kt
+++ composeApp/src/commonMain/kotlin/my/daily/pulse/di/ViewModelsModule.kt
@@ -1,10 +1,12 @@
 package my.daily.pulse.di
 
 import my.daily.pulse.articles.presentation.ArticlesViewModel
+import my.daily.pulse.sources.presentation.SourcesViewModel
 import org.koin.compose.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelsModule = module {
 
     viewModel { ArticlesViewModel(get()) }
+    viewModel { SourcesViewModel(get()) }
 }
--- shared/src/iosMain/kotlin/my/daily/pulse/di/KoinInitializer.kt
+++ shared/src/iosMain/kotlin/my/daily/pulse/di/KoinInitializer.kt
@@ -1,6 +1,7 @@
 package my.daily.pulse.di
 
 import my.daily.pulse.articles.presentation.ArticlesViewModel
+import my.daily.pulse.sources.presentation.SourcesViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.core.context.startKoin
@@ -15,4 +16,9 @@ fun initKoin() {
 class ArticlesInjector : KoinComponent {
 
     val articlesViewModel: ArticlesViewModel by inject()
+}
+
+class SourcesInjector : KoinComponent {
+
+    val sourcesViewModel: SourcesViewModel by inject()
 }
